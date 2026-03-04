b11a7a4b0e273855c5c2f173f490469d6f9cf11c
Sebas Echazú <sebastianechazu@outlook.com>
2026-03-04 09:08:56 -0300

    feat: Refactor Firebase configuration to use a default object and streamline environment setup

--- projects/gym-library/src/lib/environments/environment.firebase.ts
+++ projects/gym-library/src/lib/environments/environment.firebase.ts
@@ -3,16 +3,22 @@ import { Environment } from '../models/environment.model';
 // @ts-ignore
 import { firebaseSecrets } from './environment.secrets';
 
+const defaultFirebase = {
+  apiKey: 'demo',
+  authDomain: 'demo.firebaseapp.com',
+  projectId: 'demo-project',
+  storageBucket: 'demo-project.appspot.com',
+  messagingSenderId: '123456789',
+  appId: '1:123456789:web:demo',
+  measurementId: 'G-DEMO',
+};
+
+const firebaseConfig = (firebaseSecrets && firebaseSecrets.apiKey && firebaseSecrets.apiKey.length > 0)
+  ? firebaseSecrets
+  : defaultFirebase;
+
 export const firebaseEnvironment: Environment = {
   production: false,
   useEmulator: false,
-  firebase: firebaseSecrets || {
-    apiKey: 'demo',
-    authDomain: 'demo.firebaseapp.com',
-    projectId: 'demo-project',
-    storageBucket: 'demo-project.appspot.com',
-    messagingSenderId: '123456789',
-    appId: '1:123456789:web:demo',
-    measurementId: 'G-DEMO',
-  },
+  firebase: firebaseConfig,
 };
--- projects/gym-library/src/lib/environments/environment.ts
+++ projects/gym-library/src/lib/environments/environment.ts
@@ -3,19 +3,25 @@ import { Environment } from '../models/environment.model';
 // @ts-ignore
 import { firebaseSecrets } from './environment.secrets';
 
+const defaultFirebase = {
+  apiKey: 'demo',
+  authDomain: 'demo.firebaseapp.com',
+  projectId: 'default-project',
+  storageBucket: 'default-project.appspot.com',
+  messagingSenderId: '123456789',
+  appId: '1:123456789:web:demo',
+  measurementId: 'G-DEMO',
+};
+
+const chosenFirebase = (firebaseSecrets && firebaseSecrets.apiKey && firebaseSecrets.apiKey.length > 0)
+  ? firebaseSecrets
+  : defaultFirebase;
+
 export const developmentEnvironment: Environment = {
   production: false,
   useEmulator: true,
   firebase: {
-    ...(firebaseSecrets || {
-      apiKey: 'demo',
-      authDomain: 'demo.firebaseapp.com',
-      projectId: 'default-project',
-      storageBucket: 'default-project.appspot.com',
-      messagingSenderId: '123456789',
-      appId: '1:123456789:web:demo',
-      measurementId: 'G-DEMO',
-    }),
+    ...chosenFirebase,
     // Cuando usamos emuladores, forzamos el projectId para que coincida con los emuladores
     projectId: 'default-project',
     storageBucket: 'default-project.appspot.com',
